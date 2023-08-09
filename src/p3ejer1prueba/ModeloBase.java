2cad906d4c6e655976574516a6482e029a275141
LalauDim <141686155+LalauDim@users.noreply.github.com>
2023-08-09 19:33:41 -0300

    inicio
    Agrego atributos, gets y sets

--- src/p3ejer1prueba/ModeloBase.java
+++ src/p3ejer1prueba/ModeloBase.java
@@ -5,10 +5,32 @@
  */
 package p3ejer1prueba;
 
-/**
- *
- * @author LAu
- */
+
 public class ModeloBase {
+    
+    private String nombre;
+    private int edad;
+
+    public ModeloBase(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
     
 }
