0113d055e3230255a984bc8afde8ca6cfb7da249
LalauDim <141686155+LalauDim@users.noreply.github.com>
2023-08-09 19:17:23 -0300

    Initial commit

---
+++ src/p3ejer1prueba/P3Ejer1Prueba.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p3ejer1prueba;
+
+/**
+ *
+ * @author LAu
+ */
+public class P3Ejer1Prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
